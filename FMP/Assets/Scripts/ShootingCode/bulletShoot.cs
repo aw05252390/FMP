83dbc8b88873bc072dedb7b5362da467ea9a94ad
aw05252390 <aw05252390@priestley.ac.uk>
2021-03-25 07:46:38 +0000

    Added AStartMenU

---
+++ FMP/Assets/Scripts/ShootingCode/bulletShoot.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class bulletShoot : MonoBehaviour
+{
+    public float Timer;
+    public Transform TurretfirePoint;
+    public GameObject TurretbulletPrefab;
+
+    public float bulletForce;
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        Timer = 2;
+    }
+
+    void Update()
+    {
+        Timer -= 1 * Time.deltaTime;
+        if (Timer < 0)
+        {
+            Shoot();
+            Timer = 2;
+        }
+
+    }
+    void Shoot()
+    {
+        GameObject bullet = Instantiate(TurretbulletPrefab, TurretfirePoint.position, TurretfirePoint.rotation);
+        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+        rb.AddForce(TurretfirePoint.up * bulletForce, ForceMode2D.Impulse);
+    }
+}
+
+
